8e0ba6d883e36b305a3da66aa1a02b38833a8e4b
Nikita Belonogov <waksaone@gmail.com>
2022-04-21 13:56:20 +0000

    label-studio: add 1.4.1.post1 bottle.

--- Formula/label-studio.rb
+++ Formula/label-studio.rb
@@ -7,6 +7,13 @@ class LabelStudio < Formula
   sha256 "d651c365fad7e971ca7129f3a74943979449893fbcd71fc4dde0932e4e45e787"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://github.com/heartexlabs/homebrew-tap/releases/download/label-studio-1.4.1.post1"
+    rebuild 2
+    sha256 big_sur:  "3e4095650db01bd3d34186fa6e62c5da1a86fa3488995864954b3d4152f40f0a"
+    sha256 catalina: "1d09cadffd1c6ec931cdcb5ef7c6bb51458d9bffc4b9dc24c877ec2b4b2b6d25"
+  end
+
   depends_on "python@3.8"
 
   def install
