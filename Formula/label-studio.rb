bc3978acee1f72b8c496ad0809e6ea45b2665603
Nikita Belonogov <nikita.belonogov@heartex.com>
2023-01-27 19:37:52 +0400

    test

--- Formula/label-studio.rb
+++ Formula/label-studio.rb
@@ -18,7 +18,7 @@ class LabelStudio < Formula
 
   def install
     venv = virtualenv_create(libexec, "python3", system_site_packages: true)
-    system libexec/"bin/pip", "install", "--verbose", "--ignore-installed", buildpath
+    system libexec/"bin/pip", "install", "--verbose", "--no-binary", ":all:", "--ignore-installed", buildpath
     system libexec/"bin/pip", "uninstall", "-y", "label-studio"
     venv.pip_install_and_link buildpath
   end
