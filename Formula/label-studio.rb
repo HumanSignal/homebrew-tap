8d1bd182fa7b6122790be280b55e1f70b23a226c
Nikita Belonogov <nikita.belonogov@heartex.com>
2023-01-30 01:15:31 +0400

    Pin pip

--- Formula/label-studio.rb
+++ Formula/label-studio.rb
@@ -18,6 +18,7 @@ class LabelStudio < Formula
 
   def install
     venv = virtualenv_create(libexec, "python3", system_site_packages: true)
+    venv.pip_install "pip==22.3.1"
     system libexec/"bin/pip", "install", "--verbose", "--ignore-installed", buildpath
     system libexec/"bin/pip", "uninstall", "-y", "label-studio"
     venv.pip_install_and_link buildpath
