582695faf8ac45c07a26aeb1078c109763fd987c
Nikita Belonogov <nikita.belonogov@heartex.com>
2023-01-30 01:45:06 +0400

    fix

--- Formula/label-studio.rb
+++ Formula/label-studio.rb
@@ -17,7 +17,7 @@ class LabelStudio < Formula
   depends_on "python@3.8"
 
   def install
-    system libexec/"bin/pip", "install" "pip==22.3.1"
+    system libexec/"bin/pip", "install", "pip==22.3.1"
     venv = virtualenv_create(libexec, "python3", system_site_packages: true)
     system libexec/"bin/pip", "install", "--verbose", "--ignore-installed", buildpath
     system libexec/"bin/pip", "uninstall", "-y", "label-studio"
