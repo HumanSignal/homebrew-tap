96ac22a319fad74cba00ec386dd7c519b883ec96
Nikita Belonogov <nikita.belonogov@heartex.com>
2023-01-30 15:03:18 +0400

    Update label-studio.rb

--- Formula/label-studio.rb
+++ Formula/label-studio.rb
@@ -9,8 +9,9 @@ class LabelStudio < Formula
 
   bottle do
     root_url "https://github.com/heartexlabs/homebrew-tap/releases/download/label-studio-1.7.1"
-    sha256 monterey: "5c046429ac20ecee92551ceb18322b327148c9d660b3dad0e12b43911f7273e3"
-    sha256 big_sur:  "e4181ef608ecff3d1b99e040a6d9042bb46f3d843b417e241a6ed4ed4cdeb2ad"
+    sha256 arm64_ventura: "64e827e2449aeefd5184d0ad17e764a26094804b8d13efc9586ab1a9fd94a53d"
+    sha256 monterey:      "5c046429ac20ecee92551ceb18322b327148c9d660b3dad0e12b43911f7273e3"
+    sha256 big_sur:       "e4181ef608ecff3d1b99e040a6d9042bb46f3d843b417e241a6ed4ed4cdeb2ad"
   end
 
   depends_on "postgresql@14"
