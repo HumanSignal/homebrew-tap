005645d5e646666796710f321e52c063019b60a1
Nikita Belonogov <nikita.belonogov@heartex.com>
2023-03-21 18:00:38 +0400

    Add arm64 ventura bottle (#13)
    * Add arm64 ventura bottle
    
    * syntax

--- Formula/label-studio.rb
+++ Formula/label-studio.rb
@@ -9,8 +9,9 @@ class LabelStudio < Formula
 
   bottle do
     root_url "https://github.com/heartexlabs/homebrew-tap/releases/download/label-studio-1.7.2"
-    sha256 cellar: :any, monterey: "f0dde1e74d16e68c14e8d3092b4b9e7dd1e0f008d9a62ac163874abd0a3414fe"
-    sha256 cellar: :any, big_sur:  "7fb37cba6323316e88d1463f100e611fe5b60fa032faf67f0217d1a44fe656bc"
+    sha256               arm64_ventura: "ce4a373e379645ae8df00e602f37bb18c2ca72595a4768ff5fce2b06ac19b410"
+    sha256 cellar: :any, monterey:      "f0dde1e74d16e68c14e8d3092b4b9e7dd1e0f008d9a62ac163874abd0a3414fe"
+    sha256 cellar: :any, big_sur:       "7fb37cba6323316e88d1463f100e611fe5b60fa032faf67f0217d1a44fe656bc"
   end
 
   depends_on "postgresql@14"
