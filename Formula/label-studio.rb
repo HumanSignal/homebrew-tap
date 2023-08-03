92dcc8655467a1977756410cb1a35a8fb10d06a9
Sergey Zhuk <sergey.zhuk@humansignal.com>
2023-08-03 19:38:17 +0100

    restore original formula

--- Formula/label-studio.rb
+++ Formula/label-studio.rb
@@ -16,7 +16,8 @@ class LabelStudio < Formula
   depends_on "python@3.10" # Apple's Pypthon distribution does not include pip
 
   def install
-    venv = virtualenv_create(libexec, "python3", system_site_packages: true, without_pip: false)
+    venv = virtualenv_create(libexec, "python3", system_site_packages: true)
+    system libexec/"bin/pip", "install", "--verbose", "--upgrade", "pip==22.3.1"
     system libexec/"bin/pip", "install", "--verbose", "--ignore-installed", buildpath
     system libexec/"bin/pip", "uninstall", "-y", "label-studio"
     venv.pip_install_and_link buildpath
