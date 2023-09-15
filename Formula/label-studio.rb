28f4116e6f245e0e17f53a6ccc5d8b950d026de5
Sergey Zhuk <farioas@gmail.com>
2023-09-15 18:05:58 +0000

    label-studio: update 1.8.2.post0 bottle.

--- Formula/label-studio.rb
+++ Formula/label-studio.rb
@@ -8,8 +8,8 @@ class LabelStudio < Formula
   license "Apache-2.0"
 
   bottle do
-    root_url "https://github.com/HumanSignal/homebrew-tap/releases/download/label-studio-1.8.2"
-    sha256 ventura: "1acc1c48ac0d4f7708c03f6157b7ccfccc025df46aabca24ab54487f33037934"
+    root_url "https://github.com/HumanSignal/homebrew-tap/releases/download/label-studio-1.8.2.post0"
+    sha256 ventura: "531639f4d174ba71212edb05fd43ecfeca5978db319e6535659c53334206d5a0"
   end
 
   depends_on "postgresql@14"
