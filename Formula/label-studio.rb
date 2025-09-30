2632b2e59e692d950c1e63fcdff5c6902a4bbe89
Nikita Belonogov <nikita.belonogov@humansignal.com>
2025-09-30 16:39:21 +0400

    test

--- Formula/label-studio.rb
+++ Formula/label-studio.rb
@@ -7,13 +7,6 @@ class LabelStudio < Formula
   sha256 "1ae8c0c3430ee023b89ad0a4d994fe08ff6ff46de6b3566781a0dd54f521c5ba"
   license "Apache-2.0"
 
-  bottle do
-    root_url "https://github.com/HumanSignal/homebrew-tap/releases/download/label-studio-1.20.0"
-    sha256 arm64_sequoia: "b940bf1c6029ef4e135e537083629f108d6ca7fff77113def1b8b8f28cc12bf2"
-    sha256 arm64_sonoma:  "7775555056ed984de11127867a48a098c79ca07a0f2a5201ea1f8855bcf3f9cf"
-    sha256 ventura:       "deb442029400c58c00b8d83ba182571433fb74478ece0ab407bffddf7b51151b"
-  end
-
   depends_on "postgresql@14"
   depends_on "python@3.10" # Apple's Pypthon distribution does not include pip
 
