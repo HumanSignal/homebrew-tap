02b6aa0be0c7c7587fbcb97df919dbd726b66061
Nikita Belonogov <waksaone@gmail.com>
2026-01-07 03:41:39 +0400

    add uses_from_macos

--- Formula/label-studio.rb
+++ Formula/label-studio.rb
@@ -17,6 +17,9 @@ class LabelStudio < Formula
   depends_on "postgresql@14"
   depends_on "python@3.10" # Apple's Python distribution does not include pip
 
+  uses_from_macos "libxml2"
+  uses_from_macos "libxslt"
+
   resource "annotated-types" do
     url "https://files.pythonhosted.org/packages/ee/67/531ea369ba64dcff5ec9c3402f9f51bf748cec26dde048a2f973a4eea7f5/annotated_types-0.7.0.tar.gz"
     sha256 "aff07c09a53a08bc8cfccb9c85b05f1aa9a2a6f23728d790723543408344ce89"
